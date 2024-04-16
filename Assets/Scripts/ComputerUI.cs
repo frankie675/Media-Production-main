7581c74b5ada412ef1984d6b9f73e6bcacf8aa5d
frankie675 <114918110+frankie675@users.noreply.github.com>
2024-04-16 18:05:22 +0100

    update door still not working

--- Assets/Scripts/ComputerUI.cs
+++ Assets/Scripts/ComputerUI.cs
@@ -1,59 +1,101 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ComputerUI : MonoBehaviour
 {
-    // Start is called before the first frame update
     public GameObject ui;
+    public string correctPassword = "password"; // Change this to your correct password
+    private InputField passwordInputField;
+    private bool isUIVisible = false;
+
     void Start()
     {
         HideUI();
-         Debug.Log("Computer script running");
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        // Find the InputField component within the children of the UI GameObject
+        passwordInputField = ui.GetComponentInChildren<InputField>();
+        if (passwordInputField == null)
+        {
+            Debug.LogError("InputField not found in UI GameObject.");
+        }
+        else
+        {
+            Debug.Log("InputField found: " + passwordInputField.gameObject.name);
+        }
+        Debug.Log("Computer script running");
     }
 
     private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
         {
-            Debug.Log("Collision with computer");
-            if(other.tag == "Player")
+            ShowUI();
+        }
+    }
+
+    public void HideUI()
+    {
+        ui.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        isUIVisible = false;
+    }
+
+    public void ShowUI()
+    {
+        ui.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        isUIVisible = true;
+
+        // Set the input field as focused and activated when showing the UI
+        if (passwordInputField != null)
+        {
+            passwordInputField.Select();
+            passwordInputField.ActivateInputField();
+        }
+    }
+
+    public void PressExit()
+    {
+        HideUI();
+    }
+
+    public void SubmitPassword()
+    {
+        if (passwordInputField != null)
+        {
+            string password = passwordInputField.text;
+            if (password == correctPassword)
             {
-                ShowUI();
+                CorrectAnswer();
+            }
+            else
+            {
+                WrongAnswer();
             }
         }
-public void HideUI()
-{
-    ui.SetActive(false);
-
-    Cursor.lockState = CursorLockMode.Locked;
-    Cursor.visible = false;
     }
 
-public void ShowUI()
-{
-    ui.SetActive(true);
-
-    Cursor.lockState = CursorLockMode.None;
-    Cursor.visible = true;
-    }
-public void PressExit()
+    public void OnEndEdit()
     {
-HideUI();
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            SubmitPassword();
+        }
     }
+
     public void CorrectAnswer()
     {
-
+        // Add code to trigger door animation here
+        Debug.Log("Correct password entered. Door opening...");
+        HideUI(); // Hide the UI after correct password is entered
     }
 
-        public void WrongAnswer()
+    public void WrongAnswer()
     {
-        
+        Debug.Log("Wrong password entered. Try again.");
+        // You can add feedback for the player here, like showing an error message
     }
-
 }
