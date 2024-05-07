b98cb64c4d9438c6c1a836e3a791237451ea7f84
frankie675 <114918110+frankie675@users.noreply.github.com>
2024-05-07 15:18:55 +0100

    Dial1

--- Assets/Scripts/RandomDial.cs
+++ Assets/Scripts/RandomDial.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class RandomDial : MonoBehaviour
 {
+    public AudioClip[] sounds;
+    private AudioSource source;
     // Start is called before the first frame update
     void Start()
     {
-        
+      source = GetComponent<AudioSource>();  
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+     
     }
+
+private void OnTriggerEnter(Collider other)
+{
+    source.clip = sounds[Random.Range(0, sounds.Length)];
+    source.Play();
+}
+
 }
